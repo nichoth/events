96ff2017babc652d08c0ce2a7f7960be308025a8
nichoth <nichoth@gmail.com>
2023-09-09 10:51:52 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+// @ts-check
+console.log('hello')
---
+++ test/test.js
@@ -0,0 +1,7 @@
+// @ts-check
+'use strict'
+import { test } from 'tapzero'
+
+test('find an element', async t => {
+    t.ok('example', 'example test')
+})
